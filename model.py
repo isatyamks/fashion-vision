490d7e4129523bd1ce8b797f376da70e12af1f6a
isatyamks <isatyamks@gmail.com>
2025-07-06 14:21:46 +0530

    Refactor similarity function and update model weights; add training configuration and various report files

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-#import the required libraries
 import cv2
 from ultralytics import YOLO
 from PIL import Image
@@ -6,46 +5,21 @@ import os
 import numpy as np
 from datetime import datetime
 
-from src.similar import is_similar
+from src.similar import similar
 
-#define model with the weights
-model = YOLO("weights\\epoch_1\\best.pt")
+model = YOLO("weights\\epoch_3\\best.pt")
 
-
-#for saving the crops with date_time 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 output_dir = f"video_crops\\crops_{timestamp}"
-
-
-
-
-
-
-
-
-# def is_similar(img1, img2, threshold=0.4):
-#     # Compare color histograms of two images before and after 
-#     img1 = img1.resize((100, 100)).convert("RGB")
-#     img2 = img2.resize((100, 100)).convert("RGB")
-#     h1 = cv2.calcHist([np.array(img1)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
-#     h2 = cv2.calcHist([np.array(img2)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
-#     h1 = cv2.normalize(h1, h1).flatten()
-#     h2 = cv2.normalize(h2, h2).flatten()
-#     similarity = cv2.compareHist(h1, h2, cv2.HISTCMP_CORREL)
-#     return similarity > threshold
+video_path = "data\\instagram_reels\\2025-05-31_14-01-37_UTC.mp4"
 
 def main():
     names = model.names
     conf_threshold = 0.6
-
-    # Generate a timestamped output directory
-
     os.makedirs(output_dir, exist_ok=True)
-
-    video_path = "data\\instagram_reels\\2025-05-31_14-01-37_UTC.mp4"
     cap = cv2.VideoCapture(video_path)
 
-    frame_skip = 5  # to reduce the redundant frames of the reel
+    frame_skip = 5 
     frame_count = 0
     saved_crops = []
 
@@ -70,17 +44,16 @@ def main():
             class_id = int(cls.item())
             class_name = names[class_id]
 
-            # Compare with previous crops frames
             is_dup = False
             for saved in saved_crops:
-                if is_similar(cropped, saved):
+                if similar(cropped, saved):
                     is_dup = True
                     break
             if is_dup:
                 continue
 
             saved_crops.append(cropped)
-            crop_filename = f"{output_dir}/frame{frame_count}_obj{i}_{class_name}_conf{conf:.2f}.jpg"
+            crop_filename = f"{class_name}_conf{conf:.2f}.jpg"
             cropped.save(crop_filename)
             print(f"Saved unique: {crop_filename}")
 
--- src/similar.py
+++ src/similar.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 from PIL import Image
 
-
-
-def is_similar(img1, img2, threshold=0.4):
+def similar(img1, img2, threshold=0.4):
     img1 = img1.resize((100, 100)).convert("RGB")
     img2 = img2.resize((100, 100)).convert("RGB")
+
     h1 = cv2.calcHist([np.array(img1)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
     h2 = cv2.calcHist([np.array(img2)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
+
     h1 = cv2.normalize(h1, h1).flatten()
     h2 = cv2.normalize(h2, h2).flatten()
+
     similarity = cv2.compareHist(h1, h2, cv2.HISTCMP_CORREL)
     return similarity > threshold
-
