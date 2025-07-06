be6acd7a883663ec225907e9bb60731d9f8d9729
isatyamks <isatyamks@gmail.com>
2025-07-06 13:31:28 +0530

    Refactor model.py to improve crop saving functionality and enhance image similarity comparison

--- model.py
+++ model.py
@@ -1,31 +1,51 @@
+#import the required libraries
 import cv2
 from ultralytics import YOLO
 from PIL import Image
 import os
 import numpy as np
+from datetime import datetime
 
-def is_similar(img1, img2, threshold=0.4):
-    # Compare color histograms of two images
-    img1 = img1.resize((100, 100)).convert("RGB")
-    img2 = img2.resize((100, 100)).convert("RGB")
-    h1 = cv2.calcHist([np.array(img1)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
-    h2 = cv2.calcHist([np.array(img2)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
-    h1 = cv2.normalize(h1, h1).flatten()
-    h2 = cv2.normalize(h2, h2).flatten()
-    similarity = cv2.compareHist(h1, h2, cv2.HISTCMP_CORREL)
-    return similarity > threshold
+from src.similar import is_similar
+
+#define model with the weights
+model = YOLO("weights\\epoch_1\\best.pt")
+
+
+#for saving the crops with date_time 
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+output_dir = f"video_crops\\crops_{timestamp}"
+
+
+
+
+
+
+
+
+# def is_similar(img1, img2, threshold=0.4):
+#     # Compare color histograms of two images before and after 
+#     img1 = img1.resize((100, 100)).convert("RGB")
+#     img2 = img2.resize((100, 100)).convert("RGB")
+#     h1 = cv2.calcHist([np.array(img1)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
+#     h2 = cv2.calcHist([np.array(img2)], [0, 1, 2], None, [8, 8, 8], [0, 256]*3)
+#     h1 = cv2.normalize(h1, h1).flatten()
+#     h2 = cv2.normalize(h2, h2).flatten()
+#     similarity = cv2.compareHist(h1, h2, cv2.HISTCMP_CORREL)
+#     return similarity > threshold
 
 def main():
-    model = YOLO("weights\\best.pt")
     names = model.names
     conf_threshold = 0.6
-    output_dir = "video_crops_unique1"
+
+    # Generate a timestamped output directory
+
     os.makedirs(output_dir, exist_ok=True)
 
-    video_path = "data\\videos\\2025-05-31_14-01-37_UTC.mp4"
+    video_path = "data\\instagram_reels\\2025-05-31_14-01-37_UTC.mp4"
     cap = cv2.VideoCapture(video_path)
 
-    frame_skip = 5  # Process every 5th frame
+    frame_skip = 5  # to reduce the redundant frames of the reel
     frame_count = 0
     saved_crops = []
 
@@ -50,7 +70,7 @@ def main():
             class_id = int(cls.item())
             class_name = names[class_id]
 
-            # Compare with previous crops
+            # Compare with previous crops frames
             is_dup = False
             for saved in saved_crops:
                 if is_similar(cropped, saved):
